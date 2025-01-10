f48e8323e49f81972106ef1175b78f55347d241e
Ludwig Prager <lprager2013@gmail.com>
2025-01-10 19:21:01 +0100

    1st

---
+++ container/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -em
+
+#/usr/local/bin/supertuxkart --lan-server=test --network-console
+
+#sleep infinity
+
+# Log in with username and password if provided
+if [[ -n ${USERNAME} ]] && [[ -n ${PASSWORD} ]]
+then
+    supertuxkart --init-user --login=${USERNAME} --password=${PASSWORD}
+fi
+
+# Start stk server in background
+supertuxkart --server-config=server_config.xml &
+#supertuxkart --server-config=server_config.xml --lan-server=test --network-console &
+
+# Add ai kart background process if necessary
+if [[ -n ${AI_KARTS} ]]
+then
+    SERVER_PASSWORD=`cat server_config.xml | grep private-server-password | awk -F '"' '{print $2}'`
+    supertuxkart --connect-now=127.0.0.1:2759 --server-password=$SERVER_PASSWORD --network-ai=${AI_KARTS} &
+fi
+
+# Bring server process back into foreground
+fg %1
